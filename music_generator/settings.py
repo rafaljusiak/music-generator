1b3152941049be203e11d988e73ed8fb41e837e2
Rafal Jusiak <jusiak.raf@gmail.com>
2020-03-17 23:00:52 +0100

    static root

--- music_generator/settings.py
+++ music_generator/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(ROOT_DIR, "static/")
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
